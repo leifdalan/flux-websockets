bd2b0a3144872087789c6e19f4e0a973126bb7e8
Leif Dalan <leifdalan@gmail.com>
2015-09-10 14:00:30 -0700

    Added more console logging to figure out signup failures.

--- config/passport.js
+++ config/passport.js
@@ -115,6 +115,7 @@ export default function(passport) {
       passReqToCallback: true
       /*eslint-disable*/
     }, (req, username, password, done) => {
+      console.log('username', username);
       /*eslint-enable*/
       debug('LOCAL SIGNUP');
       if (username) {
@@ -122,54 +123,53 @@ export default function(passport) {
       }
 
       process.nextTick(() => {
-        // if the user is not already logged in:
-        if (!req.user || req.url === '/admin/users') {
-          User.findOne({
-            'local.username': username
-          }, (err, user) => {
+        console.log('nextTick');
+        User.findOne({
+          'local.username': username
+        }, (err, user) => {
 
-            // if there are any errors, return the error
-            if (err) {
-              debug('Signup Error.');
-              return done(err);
-            }
+          // if there are any errors, return the error
+          if (err) {
+            debug('Signup Error.');
+            return done(err);
+          }
 
-            // check to see if theres already a user with that username
-            if (user) {
-              debug('Signup Error, user already exists.');
-              return done(
-                `${username} already exists.`, false,
-                req.flash('signupMessage', 'That username is already taken.'));
+          // check to see if theres already a user with that username
+          if (user) {
+            debug('Signup Error, user already exists.');
+            return done(
+              `${username} already exists.`, false,
+              req.flash('signupMessage', 'That username is already taken.'));
+          /*eslint-disable*/
+          } else {
+          /*eslint-enable*/
+            // create the user
+            var newUser = new User();
+
+            newUser.local.username = username;
+            newUser.local.password = newUser.generateHash(password);
+            newUser.loginToken = newUser.generateToken();
+            newUser.userLevel = req.body.userLevel;
             /*eslint-disable*/
-            } else {
+            newUser.save((saveErr) => {
+              if (saveErr) {
+                return done(saveErr);
+                debug('User saving failed.', saveErr);
+              }
+              return done(null, newUser);
+            });
             /*eslint-enable*/
-              // create the user
-              var newUser = new User();
+          }
 
-              newUser.local.username = username;
-              newUser.local.password = newUser.generateHash(password);
-              newUser.loginToken = newUser.generateToken();
-              newUser.userLevel = req.body.userLevel;
-              /*eslint-disable*/
-              newUser.save((saveErr) => {
-                if (saveErr) {
-                  return done(saveErr);
-                  debug('User saving failed.', saveErr);
-                }
-                return done(null, newUser);
-              });
-              /*eslint-enable*/
-            }
-
-          });
+        });
           // if the user is logged in but has no local account...
-        } else {
-          debug(req.user);
-          debug('already logged in?');
-          // user is logged in and already has a local account.
-          // Ignore signup.
-          return done(null, req.user);
-        }
+        // } else {
+        //   debug(req.user);
+        //   debug('already logged in?');
+        //   // user is logged in and already has a local account.
+        //   // Ignore signup.
+        //   return done(null, req.user);
+        // }
       });
     })
   );
